5b065bc92356a36ba2a79d6dfca724bfcebc6ccf
riagarg <riagarg@gmail.com>
2021-01-15 23:07:33 -0800

    Updates

---
+++ precache-manifest.2f8c775d141a70f5c9181f796c7928c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d813607fdd03b222910dd88d534c0ca5",
+    "url": "/love-island-viz/index.html"
+  },
+  {
+    "revision": "6fb2e1140da293ef6744",
+    "url": "/love-island-viz/static/css/2.c8b5ab71.chunk.css"
+  },
+  {
+    "revision": "e39f33cfa1f1786c47ff",
+    "url": "/love-island-viz/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "6fb2e1140da293ef6744",
+    "url": "/love-island-viz/static/js/2.460aa5d2.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/love-island-viz/static/js/2.460aa5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39f33cfa1f1786c47ff",
+    "url": "/love-island-viz/static/js/main.2b2e2b22.chunk.js"
+  },
+  {
+    "revision": "77db3e2c02d69f08f935",
+    "url": "/love-island-viz/static/js/runtime-main.9c38f6b0.js"
+  },
+  {
+    "revision": "db80b0add92e159c78e9c0deded5b5d0",
+    "url": "/love-island-viz/static/media/amber-conflicts.db80b0ad.png"
+  },
+  {
+    "revision": "e6a322c6524ef65ca2617ef050afc0ae",
+    "url": "/love-island-viz/static/media/amber-couples.e6a322c6.png"
+  },
+  {
+    "revision": "926aeefe4547b1a20d8493c2e1173260",
+    "url": "/love-island-viz/static/media/amber-friends.926aeefe.png"
+  },
+  {
+    "revision": "f453a04cbc50b67b8b4da89656826c9e",
+    "url": "/love-island-viz/static/media/amberpic.f453a04c.png"
+  },
+  {
+    "revision": "99b13b265b550bb38c060351772fa5eb",
+    "url": "/love-island-viz/static/media/animated-cast-crop.99b13b26.png"
+  },
+  {
+    "revision": "e036c0ff84d22cb42db956656947a357",
+    "url": "/love-island-viz/static/media/animated-cast-title.e036c0ff.png"
+  },
+  {
+    "revision": "10cec6ad9e93e18d3a6fd59a7c0a5313",
+    "url": "/love-island-viz/static/media/animated-cast.10cec6ad.png"
+  },
+  {
+    "revision": "868a72e6e3029a39c0238790d1b6d5c8",
+    "url": "/love-island-viz/static/media/franpic.868a72e6.png"
+  },
+  {
+    "revision": "971d3e6fec3a80998d78e3941141dff4",
+    "url": "/love-island-viz/static/media/gregamber.971d3e6f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/love-island-viz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e71e951569bfe86b7526719927f3410",
+    "url": "/love-island-viz/static/media/mollymaetommy.2e71e951.png"
+  },
+  {
+    "revision": "c9e35c0167da900bd9fe878c753ef5f1",
+    "url": "/love-island-viz/static/media/ovie-couch.c9e35c01.jpeg"
+  }
+]);
